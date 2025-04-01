6912c0827c6ec76fde053a8ec05c490a2fea8031
max <mdsiurds@gmail.com>
2025-04-01 21:01:20 +0200

    regis push1

--- minishell.c
+++ minishell.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello from Minishell!\n");
-    printf("Modification depuis feature/parser\n");
+	printf("Hello from Minishell!\n");
+	printf("Modification depuis feature/parser\n");
 	ici cest master; iwrjerowefgjet
-    return 0;
+	return 0;
 }
 
 
 bla is la
 
 parsing fini
+
+
+regis ici
